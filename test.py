033ffb4a62a3857f1e956ec8cbde75c9b32aef15
Jessica Jones <jessica.douglas.jones@gmail.com>
2024-09-08 21:48:12 -0500

    The good news is that I think I got the multiprocessing part of the program down, and I optimized the code as much as I could. The bad news is that the program takes up just about the entirety of the RAM and crashes, so I need to figure out how to fix that.

--- test.py
+++ test.py
@@ -1,145 +1,110 @@
-from itertools import permutations
-from collections import Counter
-from line_profiler import LineProfiler
-import numpy as np
-import threading
+from itertools import permutations, combinations, batched
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from multiprocessing import cpu_count, Manager
+from collections.abc import Iterable
+from collections import OrderedDict, defaultdict, Counter
 
-def subset_sum(numbers, target, partial = [], partial_sum=0):
-    if partial_sum == target:
-        yield partial
-    if partial_sum >= target:
-        return
-    for i, n in enumerate(numbers):
-        remaining = numbers[i + 1:]
-        yield from subset_sum(remaining, target, partial + [n], partial_sum + n)
-            
-def boolean_indexing(v, fillval=np.nan):
-    lens = np.array([len(item) for item in v], np.int_)
-    mask = lens[:,None] > np.arange(lens.max())
-    out = np.full(mask.shape,fillval,np.int_)
-    out[mask] = np.concatenate(v)
-    return out
-       
-def subset_sum_range(min_limit, max_limit, num_pools):
-    num_sum_list = dict()
-    
-    for num in range(min_limit, max_limit + 1):
-        num_sum_list[num] = []
-        sub_sum = []
-        [sub_sum.append(n) for n in subset_sum(num_pools, num) if n not in sub_sum]
-        if len(sub_sum) != 0:
-            for possible_sum in sub_sum:
-                if num not in num_sum_list.keys() or not np.array_equal(possible_sum, num_sum_list[num]):
-                    num_sum_list[num].append(possible_sum)
-    
-    return num_sum_list
+def find_combinations_for_each_input(perm_input: tuple, min_limit: int, num_dict: OrderedDict, num_of_results: int):
+    sum_list, input_mask, possible_sum_list = [], [], []
+    input_sum = sum(perm_input)
 
-def does_list_contain_list(input_list, checked_list, type):
-    if type == 0:
-        for e in input_list:
-            if e == checked_list[:len(e)]:
-                return True
-    elif type == 1:
-        for n in input_list:
-            for r in input_list[n]:
-                if any(np.isin(checked_list, r)):
-                    return True
-    elif type == 2:
-        for r in input_list:
-            if r[0] == checked_list:
-                return True
-    elif type == 3:
-        for i in input_list:
-            for p in permutations(i[1]):
-                checked_list_copy = np.copy(checked_list)
-                for r in p:
-                    if np.array_equal(r, checked_list_copy[:len(r)]):
-                        checked_list_copy = checked_list_copy[len(r):]
-                if len(checked_list_copy) == 0:
-                    return True
-    return False
+    def update_lists_and_removed_count(number: int, possible_sum: tuple):
+        possible_sum_len = len(possible_sum)
+        possible_sum_list_len = len(possible_sum_list)
 
-def add_sum_from_input(possible_sum, number, min_limit, input, input_mask, sum_list, possible_sum_list):
-    input_copy = input
-    old_input_copy = np.copy(input_copy)
-    
-    while np.array_equal(np.sort(possible_sum), np.sort(input_copy[:len(possible_sum)])):
-        [input_mask.append(element) for element in input_copy[:len(possible_sum)]]
-        input_copy = input_copy[len(possible_sum):]
-        sum_list.append(number)
+        times_to_add_number = 0
+        for i in range(possible_sum_list_len, len(perm_input), possible_sum_len):
+            if Counter(perm_input[i : i + possible_sum_len]) == Counter(possible_sum):
+                times_to_add_number += 1
+            else:
+                break
+
+        sum_list.extend([number] * times_to_add_number)
+        input_mask.extend(possible_sum * times_to_add_number)
+        possible_sum_list.extend(possible_sum * times_to_add_number)
+
+        possible_sum_total = sum(possible_sum)
+        return possible_sum_total * times_to_add_number
+
+    while len(sum_list) != num_of_results:
+        for number, possible_sums in num_dict.items():
+            for possible_sum in possible_sums:
+                input_sum -= update_lists_and_removed_count(number, possible_sum)
+
+                if perm_input and input_sum < min_limit * (num_of_results - len(sum_list)):
+                    return perm_input[:len(input_mask)]
+    return sum_list
+
+def generate_num_dict_and_permutations(pools: tuple, min_limit: int, max_limit: int):
+    pool_values = [value for value, count in pools for _ in range(count)]
+
+    def subset_sum_range():
+        sum_to_combos = defaultdict(set)
+
+        for r in range(len(pool_values) + 1):
+            for combo in combinations(pool_values, r):
+                combo_sum = sum(combo)
+                if min_limit <= combo_sum <= max_limit:
+                    sum_to_combos[combo_sum].add(combo)
+
+        return {num: sum_to_combos[num] for num in range(min_limit, max_limit + 1)}
+
+    nums_available = [value for value, count in pools for _ in range(count)]
+    num_dict = OrderedDict(sorted(subset_sum_range().items(), reverse=True))
+    perm_gen = permutations(nums_available)
+
+    return num_dict, perm_gen, len(pool_values)
+
+def find_combinations(num_dict: OrderedDict, perm_gen: Iterable, num_count: int, num_of_results: int, min_limit: int):
+    def filter_permutations(perm_batch: list, min_limit: int, num_dict: OrderedDict, num_of_results: int, results, failed_masks):
+        def is_valid_permutation(perm_input: tuple):
+            return all(mask != perm_input[:len(mask)] for mask in failed_masks if len(mask) <= len(perm_input))
         
-        if len(input_copy) != len(old_input_copy):
-            possible_sum_list.append(possible_sum)
+        for perm in perm_batch:
+            if is_valid_permutation(tuple(perm)):
+                result = find_combinations_for_each_input(perm, min_limit, num_dict, num_of_results)
+                sorted_result = sorted(result, reverse=True)
+                if len(result) == num_of_results and sorted_result not in results:
+                    print(sorted_result, flush=True)
+                    results.append(sorted_result)
+                elif len(result) != num_of_results:
+                    failed_masks.append(result)
+        return 
+
+    with Manager() as manager:
+        results = manager.list()
+        failed_masks = manager.list()
+        iterations = 0
+
+        # for perm_batch in batched(perm_gen, 1024):
+        #     filter_permutations(perm_batch,
+        #             min_limit,
+        #             num_dict,
+        #             num_of_results,
+        #             results,
+        #             failed_masks)
+        #     iterations += 1
+        #     print(iterations)
+        # return results
         
-        if len(input_copy) != 0 and np.sum(input_copy) < min_limit * (num_of_results - len(sum_list)):
-            return True, input_copy
-    return False, input_copy
+        with ProcessPoolExecutor() as pool:
+            futures = [pool.submit(
+                    filter_permutations,
+                    perm_batch,
+                    min_limit,
+                    num_dict,
+                    num_of_results,
+                    results,
+                    failed_masks
+                )
+                for perm_batch in batched(perm_gen, 128)]
 
-def find_combinations_for_each_input(failed_masks, perm_input, result, min_limit, smallest_set, num_dict, num_of_results):
-    stop_looping = False
-    if not does_list_contain_list(result, perm_input, 3):
-        if not does_list_contain_list(failed_masks, perm_input, 0):
-            sum_list = []
-            input_mask = []
-            possible_sum_list = []
-            input_copy = np.copy(perm_input)
-        
-            while len(input_copy) != 0 and does_list_contain_list(num_dict, input_copy, 1):
-                for number in reversed(num_dict):
-                    for possible_sum in num_dict[number]:
-                        stop_looping, input_copy = add_sum_from_input(possible_sum, number, min_limit, input_copy, input_mask, sum_list, possible_sum_list)
+            for future in as_completed(futures):
+                perm_result = future.result()
+                iterations += 1
+                print(iterations)
+        return list(results)
 
-                        if not stop_looping and input_copy.size != 0 and input_copy.size < smallest_set:
-                            stop_looping = True
-                        if stop_looping:
-                            break
-                    if stop_looping:
-                        break
-                if stop_looping:
-                    failed_masks.append(perm_input[:len(input_mask)])
-                    break
-            sorted_sum_list = sorted(sum_list, reverse=True)
-            
-            if len(sorted_sum_list) == num_of_results and (len(result) == 0 or not does_list_contain_list(result, sorted_sum_list, 2)):
-                result.append([sorted_sum_list, possible_sum_list])
-                                         
-def find_combinations(permutations, num_dict, num_of_results, min_limit, smallest_set):
-    result = []
-    failed_masks = []
-    iterations = 0
-    lp = LineProfiler()
-    for perm_input in permutations:
-        
-        #lp_wrapper = lp(does_list_contain_list)
-        #lp_wrapper(result, perm_input, 3)
-        #lp.print_stats()
-                    
-        find_combinations_for_each_input(failed_masks, perm_input, result, min_limit, smallest_set, num_dict, num_of_results)
-        
-        iterations += 1
-        print(iterations)
-    return result
-
-min_limit = 12
-max_limit = 20
-num_of_results = 6
-
-four_pool = [4 for _ in range(7)]
-five_pool = [5 for _ in range(5)]
-six_pool = [6 for _ in range(6)]
-
-nums_available = np.array(four_pool + five_pool + six_pool)
-sum_of_pools = sum(nums_available)
-
-perm = permutations(nums_available)
-num_dict = subset_sum_range(min_limit, max_limit, nums_available)
-
-smallest_set = len(next(iter(num_dict.values())))
-
-for n in num_dict:
-    for s in num_dict[n]:
-        if len(s) < smallest_set:
-            smallest_set = len(s)
-     
-print(find_combinations(perm, num_dict, num_of_results, min_limit, smallest_set))
-#print(num_list)
+if __name__ == '__main__':
+    num_dict, perm_gen, num_count = generate_num_dict_and_permutations(((4, 7), (5, 5), (6, 6)), 12, 20)
+    print(find_combinations(num_dict, perm_gen, num_count, 6, 12))
